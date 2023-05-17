782f778135292b0df05cb6dd81bf62867a4abd66
Hugo Tell <hugo.tell@gmail.com>
2023-05-17 18:25:35 +0200

    .

--- python/acc_tank.py
+++ python/acc_tank.py
@@ -1,17 +1,22 @@
 import librtd
 import paho.mqtt.client as mqtt
 import time
+import statistics
 
 mqttBroker ="192.168.0.110" 
 
 client = mqtt.Client("RTD")
-client.connect(mqttBroker) 
+client.connect(mqttBroker)
+rtd1 = [0,0,0,0,0,0,0,0,0,0]
 
 while True:
     i = 1
-    while i < 8:
-        rtd = librtd.get(0, i)
-        client.publish("test/test2", rtd)
-        print("Just published " + str(rtd) + " to topic test/test2")
+    while i < 10:
+        rtd1[i] = librtd.get(0, 1)
+        mean_rtd1 = statistics.mean(rtd1)
+        client.publish("test/test2", mean_rtd1)
+        print("Just published " + str(mean_rtd1) + " to topic test/test2")
+        print(rtd1)
+        print(mean_rtd1)
         i += 1
         time.sleep(1)
