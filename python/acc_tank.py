e11adaf5e2725cf69b8007cec01df16e132eb4f2
Hugo Tell <hugo.tell@gmail.com>
2023-05-17 17:14:57 +0200

    .

--- python/acc_tank.py
+++ python/acc_tank.py
@@ -6,8 +6,8 @@ import time
 while True:
     i = 1
     while i < 8:
-    rtd[i] = librtd.get(0, i)
-    client.publish("test/test2", rtd[i])
-    print("Just published " + str(rtd[i]) + " to topic test/test2")
-    i += 1
-    time.sleep(1)
+        rtd = librtd.get(0, i)
+        client.publish("test/test2", rtd)
+        print("Just published " + str(rtd) + " to topic test/test2")
+        i += 1
+        time.sleep(1)
