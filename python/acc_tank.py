6351b89c9e5f94100ac980140e0503d4304e5f39
DonHugo <hugo.tell@gmail.com>
2023-05-22 15:19:11 +0200

    .

--- python/acc_tank.py
+++ python/acc_tank.py
@@ -17,9 +17,10 @@ def read_rtd(board_id,rtd_id,loops):
         rtd1[i] = librtd.get(board_id, rtd_id)
         mean_rtd1 = round(statistics.mean(rtd1),1)
         client.publish("test/test2", mean_rtd1)
-        print("Just published " + str(mean_rtd1) + " to topic test/test2")
+        #print("Just published " + str(mean_rtd1) + " to topic test/test2")
         #print(rtd1)
-        print(mean_rtd1)
+        print("rtd" + srt(rtd_id) + " " + str(mean_rtd1))
+        #print(mean_rtd1)
         i += 1
         time.sleep(1)
 
@@ -27,3 +28,4 @@ while True:
 #    a = 0
 #    while a < 8
     read_rtd(0,1,10)
+    read_rtd(0,2,10)
