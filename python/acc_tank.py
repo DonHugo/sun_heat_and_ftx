e1d85994d252107a403af2e898fc07e1f415a8e7
DonHugo <hugo.tell@gmail.com>
2023-05-22 15:50:18 +0200

    .

--- python/acc_tank.py
+++ python/acc_tank.py
@@ -1,16 +1,32 @@
-import thread
+import threading
 import librtd
 import paho.mqtt.client as mqtt
 import time
 import statistics
 
+#MQTT variables
 mqttBroker ="192.168.0.110" 
-
 client = mqtt.Client("RTD")
 client.connect(mqttBroker)
+
+# Threding variables
+exitFlag = 0
+
+
+# Application variables
 rtd1 = [0,0,0,0,0,0,0,0,0,0]
 loops = 10
 
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print "Starting " + self.name
+      print_time(self.name, 5, self.counter)
+      print "Exiting " + self.name
 
 def read_rtd(board_id,rtd_id,loops):
     i = 0
@@ -25,12 +41,13 @@ def read_rtd(board_id,rtd_id,loops):
         i += 1
         time.sleep(1)
 
-# Create two threads as follows
-try:
-   thread.start_new_thread( print_time, ("Thread-1", 2, ) )
-   thread.start_new_thread( print_time, ("Thread-2", 4, ) )
-except:
-   print "Error: unable to start thread"
+# Create new threads
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# Start new Threads
+thread1.start()
+thread2.start()
 
 while True:
 #    a = 0
