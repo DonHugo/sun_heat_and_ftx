59a342d868bbadfb335d9b0dcb3277b0113766ce
DonHugo <hugo.tell@gmail.com>
2023-05-22 17:14:57 +0200

    .

--- python/acc_tank.py
+++ python/acc_tank.py
@@ -14,7 +14,7 @@ exitFlag = 0
 
 
 # Application variables
-rtd_1 = [0,0,0,0,0,0,0,0,0,0]
+rtd = [0,0,0,0,0,0,0,0,0,0]
 #rtd_2 = [0,0,0,0,0,0,0,0,0,0]
 #rtd_3 = [0,0,0,0,0,0,0,0,0,0]
 rtd_avg = [0,0,0,0,0,0,0,0]
@@ -23,10 +23,11 @@ loops = 10
 def read_rtd(board_id,rtd_id,loops):
     i = 0
     while i < loops:
-        rtd_1[i] = librtd.get(board_id, rtd_id)
-        avg_rtd_1 = round(statistics.mean(rtd_1),1)
+        rtd[i] = librtd.get(board_id, rtd_id)
+        avg_rtd_1 = round(statistics.mean(rtd),1)
         client.publish("test/test2", avg_rtd_1)
-        rtd_avg[rtd_id-1] = avg_rtd_1
+        rtd_position = rtd_id-1
+        rtd_avg[rtd_position] = avg_rtd_1
         #print("Just published " + str(mean_rtd1) + " to topic test/test2")
         #print(rtd1)
         print("rtd_" + str(rtd_id) + " " + str(avg_rtd_1))
@@ -37,6 +38,7 @@ a=1
 while True:
     read_rtd(0,a,10)
     print(rtd_avg[a])
+    print(rtd_avg)
     a += 1
     if a > 2:
         a=0
