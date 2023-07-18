e0437c94477f09b592f6a49734c3e5b189aff1fa
DonHugo <hugo.tell@gmail.com>
2023-07-18 14:44:27 +0200

    .

--- python/megabas_mqtt.py
+++ python/megabas_mqtt.py
@@ -134,6 +134,7 @@ def read_megabas_1k(stack, input):
         elif mod_sensor >= limit[15] and mod_sensor < limit[16]:      temp = calc_temp(mod_sensor-limit[15],delta[15],150)
         elif mod_sensor >= limit[16] and mod_sensor < limit[17]:      temp = calc_temp(mod_sensor-limit[16],delta[16],160)
         elif mod_sensor >= limit[17] and mod_sensor < limit[18]:      temp = calc_temp(mod_sensor-limit[17],delta[17],170)   
+    print("===== def get_temp =====")
     print(sensor)
     print(mod_sensor)
     print(temp)
@@ -194,9 +195,11 @@ def collect_sensor_data_rtd(stack,input,iterations):
 
 def collect_sensor_data_mega(stack,input,iterations):
     i = 0
+    print("===== def collect_sensor_data_mega =====")
     while i < iterations:
         collection[i] = read_megabas_1k(stack, input)
         if isinstance(collection[i], (float, int)) and collection[i] != 9999:
+            print("===== if =====")
             print(collection[i])
             avg_value = round(statistics.mean(collection),1)
             print(avg_value)
@@ -209,7 +212,8 @@ def collect_sensor_data_mega(stack,input,iterations):
             #print(rtd1)
             ##print("rtd_" + str(rtd_id) + " " + str(avg_rtd_1))
             #print(mean_rtd_1)
-        else: 
+        else:
+            print("===== else =====") 
             avg_value = round(statistics.mean(collection),1)
             print(avg_value)
             stack_position = stack-1
@@ -233,12 +237,12 @@ def read_onewire():
     return
 
 #get_temp()
-print(read_megabas_1k(3,6))
-print(read_megabas_1k(3,7))
-print(read_megabas_1k(3,8))
-print(read_rtd(4,1))
+#print(read_megabas_1k(3,6))
+#print(read_megabas_1k(3,7))
+#print(read_megabas_1k(3,8))
+#print(read_rtd(4,1))
 #board_megabas_values()
-read_onewire()
+#read_onewire()
 
 a=1
 while True:
