ea6a88ee5024bed112449df540e12c9d49356827
DonHugo <hugo.tell@gmail.com>
2023-07-19 13:20:57 +0200

    .

--- python/megabas_mqtt.py
+++ python/megabas_mqtt.py
@@ -45,7 +45,7 @@ while Connected != True:    #Wait for connection
 
 
 
-def calc_temp(calc, delta, deci):
+def calc_megabas_temp(calc, delta, deci):
     calculated_temp=(calc/delta)+deci
     round_calculated_temp = (round(calculated_temp, 1))
     p_calculated_temp = "calculated_temp: {}"
@@ -53,59 +53,6 @@ def calc_temp(calc, delta, deci):
     p_round_calculated_temp = "round_calculated_temp: {}"
     print(p_round_calculated_temp.format(round_calculated_temp))
     return round_calculated_temp
-
-def get_temp():
-    limit = [1000,1039,1077.9,1116.7,1155.4,1194,1232.4,1270.8,1309,1347.1,1385.1,1422.9,1460.7,1498.3,1535.8,1573.3,1610.5,1647.7,1684.8]
-    delta = [3.9,3.89,3.88,3.87,3.86,3.84,3.84,3.82,3.81,3.8,3.78,3.78,3.76,3.75,3.75,3.72,3.72,3.71]
-
-    for x in range(8):
-        sensor = m.getRIn1K(3, x+1)
-        mod_sensor = sensor*1000
-        temp = "No value"
-        sensor_name = "temp_sensor_3_{}"
-             
-        if sensor == 60:
-            print("no sensor connected!")
-            temp = 9999
-        else:
-            #print(sensor)
-            if mod_sensor >= limit[0] and mod_sensor < limit[1]:        temp = calc_temp(mod_sensor-limit[0],delta[0],0)
-            elif mod_sensor >= limit[1] and mod_sensor < limit[2]:      temp = calc_temp(mod_sensor-limit[1],delta[1],10)
-            elif mod_sensor >= limit[2] and mod_sensor < limit[3]:      temp = calc_temp(mod_sensor-limit[2],delta[2],20)
-            elif mod_sensor >= limit[3] and mod_sensor < limit[4]:      temp = calc_temp(mod_sensor-limit[3],delta[3],30)
-            elif mod_sensor >= limit[4] and mod_sensor < limit[5]:      temp = calc_temp(mod_sensor-limit[4],delta[4],40)
-            elif mod_sensor >= limit[5] and mod_sensor < limit[6]:      temp = calc_temp(mod_sensor-limit[5],delta[5],50)
-            elif mod_sensor >= limit[6] and mod_sensor < limit[7]:      temp = calc_temp(mod_sensor-limit[6],delta[6],60)
-            elif mod_sensor >= limit[7] and mod_sensor < limit[8]:      temp = calc_temp(mod_sensor-limit[7],delta[7],70)
-            elif mod_sensor >= limit[8] and mod_sensor < limit[9]:      temp = calc_temp(mod_sensor-limit[8],delta[8],80)
-            elif mod_sensor >= limit[9] and mod_sensor < limit[10]:      temp = calc_temp(mod_sensor-limit[9],delta[9],90)
-            elif mod_sensor >= limit[10] and mod_sensor < limit[11]:      temp = calc_temp(mod_sensor-limit[10],delta[10],100)
-            elif mod_sensor >= limit[11] and mod_sensor < limit[12]:      temp = calc_temp(mod_sensor-limit[11],delta[11],110)
-            elif mod_sensor >= limit[12] and mod_sensor < limit[13]:      temp = calc_temp(mod_sensor-limit[12],delta[12],120)
-            elif mod_sensor >= limit[13] and mod_sensor < limit[14]:      temp = calc_temp(mod_sensor-limit[13],delta[13],130)
-            elif mod_sensor >= limit[14] and mod_sensor < limit[15]:      temp = calc_temp(mod_sensor-limit[14],delta[14],140)
-            elif mod_sensor >= limit[15] and mod_sensor < limit[16]:      temp = calc_temp(mod_sensor-limit[15],delta[15],150)
-            elif mod_sensor >= limit[16] and mod_sensor < limit[17]:      temp = calc_temp(mod_sensor-limit[16],delta[16],160)
-            elif mod_sensor >= limit[17] and mod_sensor < limit[18]:      temp = calc_temp(mod_sensor-limit[17],delta[17],170)
-            #print(sensor_name.format(x))
-            #round_temp = (round(temp, 1))    
-            #print(round_temp)
-            print(sensor_name.format(x) + ": "+ str(temp))
-            x = {
-                "name": sensor_name.format(x),
-                "temperature": temp,
-                "ohms": sensor
-                }
-            y = json.dumps(x, ensure_ascii=False).encode('utf8')
-            print(y)
-
-            # msg = y
-            # topic_path = "/{}"
-            # topic = topic_path.format(beach)
-            # client.publish(topic,msg)
-
-            #time.sleep(1)
-    return temp
 
 def read_megabas_1k(stack, input):
     limit = [1000,1039,1077.9,1116.7,1155.4,1194,1232.4,1270.8,1309,1347.1,1385.1,1422.9,1460.7,1498.3,1535.8,1573.3,1610.5,1647.7,1684.8]
@@ -121,24 +68,24 @@ def read_megabas_1k(stack, input):
         #megabas_temp = template.format(stack,input)
         megabas_temp = 9999
     else:
-        if mod_sensor >= limit[0] and mod_sensor < limit[1]:        megabas_temp = calc_temp(mod_sensor-limit[0],delta[0],0)
-        elif mod_sensor >= limit[1] and mod_sensor < limit[2]:      megabas_temp = calc_temp(mod_sensor-limit[1],delta[1],10)
-        elif mod_sensor >= limit[2] and mod_sensor < limit[3]:      megabas_temp = calc_temp(mod_sensor-limit[2],delta[2],20)
-        elif mod_sensor >= limit[3] and mod_sensor < limit[4]:      megabas_temp = calc_temp(mod_sensor-limit[3],delta[3],30)
-        elif mod_sensor >= limit[4] and mod_sensor < limit[5]:      megabas_temp = calc_temp(mod_sensor-limit[4],delta[4],40)
-        elif mod_sensor >= limit[5] and mod_sensor < limit[6]:      megabas_temp = calc_temp(mod_sensor-limit[5],delta[5],50)
-        elif mod_sensor >= limit[6] and mod_sensor < limit[7]:      megabas_temp = calc_temp(mod_sensor-limit[6],delta[6],60)
-        elif mod_sensor >= limit[7] and mod_sensor < limit[8]:      megabas_temp = calc_temp(mod_sensor-limit[7],delta[7],70)
-        elif mod_sensor >= limit[8] and mod_sensor < limit[9]:      megabas_temp = calc_temp(mod_sensor-limit[8],delta[8],80)
-        elif mod_sensor >= limit[9] and mod_sensor < limit[10]:      megabas_temp = calc_temp(mod_sensor-limit[9],delta[9],90)
-        elif mod_sensor >= limit[10] and mod_sensor < limit[11]:      megabas_temp = calc_temp(mod_sensor-limit[10],delta[10],100)
-        elif mod_sensor >= limit[11] and mod_sensor < limit[12]:      megabas_temp = calc_temp(mod_sensor-limit[11],delta[11],110)
-        elif mod_sensor >= limit[12] and mod_sensor < limit[13]:      megabas_temp = calc_temp(mod_sensor-limit[12],delta[12],120)
-        elif mod_sensor >= limit[13] and mod_sensor < limit[14]:      megabas_temp = calc_temp(mod_sensor-limit[13],delta[13],130)
-        elif mod_sensor >= limit[14] and mod_sensor < limit[15]:      megabas_temp = calc_temp(mod_sensor-limit[14],delta[14],140)
-        elif mod_sensor >= limit[15] and mod_sensor < limit[16]:      megabas_temp = calc_temp(mod_sensor-limit[15],delta[15],150)
-        elif mod_sensor >= limit[16] and mod_sensor < limit[17]:      megabas_temp = calc_temp(mod_sensor-limit[16],delta[16],160)
-        elif mod_sensor >= limit[17] and mod_sensor < limit[18]:      megabas_temp = calc_temp(mod_sensor-limit[17],delta[17],170)   
+        if mod_sensor >= limit[0] and mod_sensor < limit[1]:        megabas_temp = calc_megabas_temp(mod_sensor-limit[0],delta[0],0)
+        elif mod_sensor >= limit[1] and mod_sensor < limit[2]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[1],delta[1],10)
+        elif mod_sensor >= limit[2] and mod_sensor < limit[3]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[2],delta[2],20)
+        elif mod_sensor >= limit[3] and mod_sensor < limit[4]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[3],delta[3],30)
+        elif mod_sensor >= limit[4] and mod_sensor < limit[5]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[4],delta[4],40)
+        elif mod_sensor >= limit[5] and mod_sensor < limit[6]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[5],delta[5],50)
+        elif mod_sensor >= limit[6] and mod_sensor < limit[7]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[6],delta[6],60)
+        elif mod_sensor >= limit[7] and mod_sensor < limit[8]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[7],delta[7],70)
+        elif mod_sensor >= limit[8] and mod_sensor < limit[9]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[8],delta[8],80)
+        elif mod_sensor >= limit[9] and mod_sensor < limit[10]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[9],delta[9],90)
+        elif mod_sensor >= limit[10] and mod_sensor < limit[11]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[10],delta[10],100)
+        elif mod_sensor >= limit[11] and mod_sensor < limit[12]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[11],delta[11],110)
+        elif mod_sensor >= limit[12] and mod_sensor < limit[13]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[12],delta[12],120)
+        elif mod_sensor >= limit[13] and mod_sensor < limit[14]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[13],delta[13],130)
+        elif mod_sensor >= limit[14] and mod_sensor < limit[15]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[14],delta[14],140)
+        elif mod_sensor >= limit[15] and mod_sensor < limit[16]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[15],delta[15],150)
+        elif mod_sensor >= limit[16] and mod_sensor < limit[17]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[16],delta[16],160)
+        elif mod_sensor >= limit[17] and mod_sensor < limit[18]:      megabas_temp = calc_megabas_temp(mod_sensor-limit[17],delta[17],170)   
     #print("===== def read_megabas_1k =====")
     #print(sensor)
     #print(mod_sensor)
@@ -234,11 +181,11 @@ def collect_sensor_data_mega(stack,input,iterations):
         p_input_array_mean = "Input_array.mean(2){},{} = {}"     
         print(p_input_array_mean.format(stack_position,input_position,input_array_mean))      
         i += 1
-        #time.sleep(0.02)
-        if input == 7 or input == 8:
-            time.sleep(5)
-        else:
-            time.sleep(0.02)
+        time.sleep(0.02)
+        #if input == 7 or input == 8:
+        #    time.sleep(5)
+        #else:
+        #    time.sleep(0.02)
     
 
 def read_onewire():
@@ -263,7 +210,7 @@ a=1
 while True:
     p_a = "a (input) = {} "
     print(p_a.format(a))
-#    collect_sensor_data_mega(3,a,10)
+    collect_sensor_data_mega(3,a,10)
     collect_sensor_data_rtd(4,a,10)
     print(input_array)
     a += 1
