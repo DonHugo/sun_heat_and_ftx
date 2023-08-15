a741ce7da7609772c8a1ba8d8377cc9537fe991e
Hugo Tell <hugo.tell@gmail.com>
2023-08-16 00:23:55 +0200

    .

--- python/temperature_monitoring.py
+++ python/temperature_monitoring.py
@@ -628,6 +628,7 @@ def main_sun_collector(client):
         global state
         global sub_state
         dT_running = 0
+        dt = 0
         #concurrent_pump_status = lib4relind.get_relay(4, 1)
         
         if args.test_mode == True:
